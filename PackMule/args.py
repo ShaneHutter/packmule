a35ea5d8776425de2169b412a2aaba77e41e2b49
Shane Hutter <shane.hutter86@gmail.com>
2019-05-11 16:00:05 +1000

    Command line arguments
    Working on argparse command line argument parsing

--- PackMule/args.py
+++ PackMule/args.py
@@ -15,12 +15,37 @@ PackMule
 
 __all__ = [ argparser , ]
 
+
 from argparse   import ArgumentParser
+
 
 def argparser():
     """
         Command line arguments for PackMule
     """
-    parser  = ArgumentParser( **PROGRAM_INFO )
+    parser              = ArgumentParser( **PROGRAM_INFO )
+    primary_swicthes    = (
+            parser.add_mutually_exclusive_group( required = True )
+            )
+    '''
+        Can I add argument groups connected to each primary switch?
+    '''
+    info                = (
+            primary_switches.add_argument_group()
+            )
+    query               = (
+            primary_switches.add_argument_group()
+            )
+    remove              = (
+            primary_switches.add_argument_group()
+            )
+    sync                = (
+            primary_switches.add_argument_group()
+            )
+    update              = (
+            primary_switches.add_argument_group()
+            )
+
+
 
     return parser.parse_args()
