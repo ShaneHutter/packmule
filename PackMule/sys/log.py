946db26eef1e0af6fa78c606abc7d072257e1500
Shane Hutter <shane.hutter86@gmail.com>
2019-05-11 15:24:31 +1000

    Early groundwork
    Everything starts somewhere

---
+++ PackMule/sys/log.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+PackMule
+    PackMule.sys.log
+
+    Written By:
+        Shane Hutter
+
+    Description:
+        Functions and attributes for file and stream loggers
+
+    License:
+        GNU GPLv3
+"""
+
